bf27b006fd635d0186cddc280ea45745965e8b92
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-06-15 16:44:58 -0600

    test: Fix unit tests
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tests/unit/sync_strategies/test_binlog.py
+++ tests/unit/sync_strategies/test_binlog.py
@@ -210,7 +210,9 @@ class TestBinlogSyncStrategy(TestCase):
             }
         }
 
-        with patch('tap_mysql.sync_strategies.binlog.write_message') as write_msg:
+        with patch('tap_mysql.sync_strategies.binlog.write_message') as write_msg, \
+                patch('tap_mysql.stream_utils.write_message', write_msg):
+
             write_msg.side_effect = lambda msg: singer_messages.append(msg)
 
             with patch('tap_mysql.sync_strategies.binlog.BinLogStreamReader',
@@ -990,7 +992,9 @@ class TestBinlogSyncStrategy(TestCase):
             }
         }
 
-        with patch('tap_mysql.sync_strategies.binlog.write_message') as write_msg:
+        with patch('tap_mysql.sync_strategies.binlog.write_message') as write_msg, \
+                patch('tap_mysql.stream_utils.write_message', write_msg):
+
             write_msg.side_effect = lambda msg: singer_messages.append(msg)
 
             with patch('tap_mysql.sync_strategies.binlog.BinLogStreamReader',
